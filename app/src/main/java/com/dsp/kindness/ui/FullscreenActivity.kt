30f325d1fc072cbe2131367f96a15ee2db3feb97
dsp <dsp.freelancer@gmail.com>
2019-09-09 00:58:57 +0300

    KN-9 Add ui layer

--- app/src/main/java/com/dsp/kindness/ui/FullscreenActivity.kt
+++ app/src/main/java/com/dsp/kindness/ui/FullscreenActivity.kt
@@ -1,15 +1,25 @@
 package com.dsp.kindness.ui
 
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.os.Bundle
 import android.os.Handler
+import android.view.MenuItem
+import android.view.Surface
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.dsp.kindness.R
 import com.dsp.kindness.domain.entity.ActEntity
 import com.dsp.kindness.domain.interactor.*
 import com.dsp.kindness.ui.log.Logger.d
+import com.dsp.kindness.ui.log.Logger.i
 import kotlinx.android.synthetic.main.activity_fullscreen.*
 import org.koin.android.ext.android.get
+
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -24,7 +34,7 @@ class FullscreenActivity : AppCompatActivity() {
         // Note that some of these constants are new as of API 16 (Jelly Bean)
         // and API 19 (KitKat). It is safe to use them, as they are inlined
         // at compile-time and do nothing on earlier devices.
-        fullscreen_content.systemUiVisibility =
+        textView_content.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LOW_PROFILE or
                     View.SYSTEM_UI_FLAG_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
@@ -35,7 +45,7 @@ class FullscreenActivity : AppCompatActivity() {
     private val mShowPart2Runnable = Runnable {
         // Delayed display of UI elements
         supportActionBar?.show()
-        fullscreen_content_controls.visibility = View.VISIBLE
+        //TODO fullscreen_content_controls.visibility = View.VISIBLE
     }
     private var mVisible: Boolean = false
     private val mHideRunnable = Runnable { hide() }
@@ -53,30 +63,62 @@ class FullscreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        i { "onCreate" }
 
         setContentView(R.layout.activity_fullscreen)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         mVisible = true
 
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreen_content.setOnClickListener { toggle() }
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        dummy_button.setOnTouchListener(mDelayHideTouchListener)
+        setupBackground()
 
         getRandomActUseCase(UseCase.None()) {
             when (it) {
                 is Left<Failure> -> d { "TODO ex=${it.a}" }
-                is Right<ActEntity> -> d { "TODO ${it.b}" }
+                is Right<ActEntity> -> textView_content.text = it.b.description
             }
+        }
+
+        // Set up the user interaction to manually show or hide the system UI.
+        textView_content.setOnClickListener { toggle() }
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        //TODO dummy_button.setOnTouchListener(mDelayHideTouchListener)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        i { "onConfigurationChanged ${newConfig!!.orientation}" }
+        setupBackground()
+    }
+
+    private fun setupBackground() {
+        var source = BitmapFactory.decodeResource(resources, R.drawable.wood)
+        source = rotateBitmap(source, getRotationDegrees(this))
+        imageView_background.setImageBitmap(source)
+    }
+
+    private fun rotateBitmap(source: Bitmap, degrees: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degrees)
+        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, false)
+    }
+
+    private fun getRotationDegrees(context: Context): Float {
+        return when ((context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.orientation) {
+            Surface.ROTATION_0 -> 0f
+            Surface.ROTATION_90 -> 90f
+            Surface.ROTATION_180 -> 180f
+            Surface.ROTATION_270 -> 270f
+            else -> 0f
         }
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
+        i { "onPostCreate" }
 
         // Trigger the initial hide() shortly after the activity has been
         // created, to briefly hint to the user that UI controls
@@ -95,7 +137,7 @@ class FullscreenActivity : AppCompatActivity() {
     private fun hide() {
         // Hide UI first
         supportActionBar?.hide()
-        fullscreen_content_controls.visibility = View.GONE
+//TODO        fullscreen_content_controls.visibility = View.GONE
         mVisible = false
 
         // Schedule a runnable to remove the status and navigation bar after a delay
@@ -105,7 +147,7 @@ class FullscreenActivity : AppCompatActivity() {
 
     private fun show() {
         // Show the system bar
-        fullscreen_content.systemUiVisibility =
+        textView_content.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         mVisible = true
@@ -113,6 +155,17 @@ class FullscreenActivity : AppCompatActivity() {
         // Schedule a runnable to display UI elements after a delay
         mHideHandler.removeCallbacks(mHidePart2Runnable)
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        i { "onOptionsItemSelected" }
+        return when (item!!.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     /**
@@ -129,18 +182,18 @@ class FullscreenActivity : AppCompatActivity() {
          * Whether or not the system UI should be auto-hidden after
          * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
          */
-        private val AUTO_HIDE = true
+        private const val AUTO_HIDE = true
 
         /**
          * If [AUTO_HIDE] is set, the number of milliseconds to wait after
          * user interaction before hiding the system UI.
          */
-        private val AUTO_HIDE_DELAY_MILLIS = 3000
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000
 
         /**
          * Some older devices needs a small delay between UI widget updates
          * and a change of the status and navigation bar.
          */
-        private val UI_ANIMATION_DELAY = 300
+        private const val UI_ANIMATION_DELAY = 300
     }
 }
